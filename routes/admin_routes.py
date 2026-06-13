7e38be414a3634ab439385acfe6f451a4ecac297
subhasw123 <subhabishoye188@gmail.com>
2026-06-13 17:27:16 +0530

    Added Admin Pannel

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,7 +1,74 @@
-from flask import Blueprint, jsonify
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    session,
+    url_for
+)
 
-admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+from database.admin_queries import (
+    get_all_diagnoses,
+    get_dashboard_stats
+)
 
-@admin_bp.route('/status')
-def status():
-    return jsonify({'admin': 'healthy'})
+admin_bp = Blueprint(
+    "admin",
+    __name__,
+    url_prefix="/admin"
+)
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin123"
+
+
+@admin_bp.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if (
+            username == ADMIN_USERNAME
+            and password == ADMIN_PASSWORD
+        ):
+
+            session["admin"] = username
+
+            return redirect(
+                url_for("admin.dashboard")
+            )
+
+    return render_template(
+        "admin.html"
+    )
+
+
+@admin_bp.route("/dashboard")
+def dashboard():
+
+    if "admin" not in session:
+        return redirect(
+            url_for("admin.login")
+        )
+
+    diagnoses = get_all_diagnoses()
+
+    stats = get_dashboard_stats()
+
+    return render_template(
+        "admin_dashboard.html",
+        diagnoses=diagnoses,
+        stats=stats
+    )
+
+@admin_bp.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect(
+        url_for("admin.login")
+    )
--- routes/diagnosis_routes.py
+++ routes/diagnosis_routes.py
@@ -1,23 +1,12 @@
 from flask import Blueprint, request, jsonify
 from services.diagnosis_service import DiagnosisService
 from ml.load_symptoms import SYMPTOMS
-from database.diagnosis_queries import (
-    get_history
-)
+
 from flask import render_template
 
 diagnosis_bp = Blueprint('diagnosis', __name__, url_prefix='/diagnosis')
 service = DiagnosisService()
 
-@diagnosis_bp.route('/history')
-def history():
-
-    rows = get_history()
-
-    return render_template(
-        "history.html",
-        rows=rows
-    )
 
 @diagnosis_bp.route('/symptoms', methods=['GET'])
 def get_symptoms():
