daab047030ba00c3e93ea128758e6a019f1bfa98
subhasw123 <subhabishoye188@gmail.com>
2026-07-03 01:11:01 +0530

    Added some features

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASE_DIR, '.env'), override=True)
 class Config:
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Subha@123"
-    MYSQL_DATABASE = "medical_assistant"
+    MYSQL_HOST = os.getenv("DB_HOST", "localhost")
+    MYSQL_PORT = os.getenv("DB_PORT", 3306)
+    MYSQL_USER = os.getenv("DB_USER", "root")
+    MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "Subha@123")
+    MYSQL_DATABASE = os.getenv("DB_NAME", "medical_assistant")
--- database/db.py
+++ database/db.py
@@ -2,11 +2,21 @@ import mysql.connector
 from config import Config
 
 def get_connection():
+    print("HOST:", Config.MYSQL_HOST)
+    print("PORT:", Config.MYSQL_PORT)
+    print("DB:", Config.MYSQL_DATABASE)
+
     connection = mysql.connector.connect(
         host=Config.MYSQL_HOST,
+        port=Config.MYSQL_PORT,
         user=Config.MYSQL_USER,
         password=Config.MYSQL_PASSWORD,
         database=Config.MYSQL_DATABASE
     )
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT DATABASE();")
+    print("Connected to:", cursor.fetchone())
+    cursor.close()
 
     return connection
